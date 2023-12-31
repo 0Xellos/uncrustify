c60f3a749c1741cb90c864ca8a847509d3613b23
Jakub Safin <jakub.safin@innovatrics.com>
2023-12-31 03:58:22 +0100

    fix misidentification of func def with attr

--- src/combine_fix_mark.cpp
+++ src/combine_fix_mark.cpp
@@ -1773,7 +1773,8 @@ void mark_function(Chunk *pc)
          && (  (  prev->IsParenClose()
                && prev->GetParentType() != CT_D_CAST
                && prev->GetParentType() != CT_MACRO_OPEN  // Issue #2726
-               && prev->GetParentType() != CT_MACRO_CLOSE)
+               && prev->GetParentType() != CT_MACRO_CLOSE
+               && prev->GetParentType() != CT_ATTRIBUTE)
             || prev->Is(CT_ASSIGN)
             || prev->Is(CT_RETURN)))
       {
